7df03927a61b314f0e66a7c25225b4e9acc4ccd3
RyoKainuma <numac.kr@gmail.com>
2021-11-16 09:05:58 +0900

    Customize Toolbar height.

--- src/src/pages/ServiceMenu.tsx
+++ src/src/pages/ServiceMenu.tsx
@@ -1,27 +1,40 @@
 import * as React from 'react';
-
+import { makeStyles } from "@material-ui/core/styles";
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
 import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
+import Paper from '@mui/material/Paper';
 import Toolbar from '@mui/material/Toolbar';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import IconButton from '@mui/material/IconButton';
 import LayersIcon from '@mui/icons-material/Layers';
 import MenuIcon from '@mui/icons-material/Menu';
-import PeopleIcon from '@mui/icons-material/People';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Calculate } from '@mui/icons-material';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
+const useStyles = makeStyles(
+  {
+    toolBar: {
+      maxHeight: 36,
+      minHeight: '24px !important',
+      padding: 0
+    }
+  }
+);
 
 const ServiceMenu = () => {
-  const drawerWidth: number = 240;
+  const drawerWidth: number = 280;
   // const [open, setOpen] = React.useState(false);
   const [open, setOpen] = React.useState(true); // should be fix
   const toggleDrawer = () => { setOpen(!open); };
@@ -51,6 +64,8 @@ const ServiceMenu = () => {
     }),
   );
 
+  const classes = useStyles();
+
   return (
     <Drawer variant='permanent' open={open}>
       <Toolbar
@@ -62,7 +77,6 @@ const ServiceMenu = () => {
         }}
       >
       </Toolbar>
-
       <IconButton
         aria-label='open drawer'
         color='inherit'
@@ -79,8 +93,69 @@ const ServiceMenu = () => {
           : <MenuIcon />
         }
       </IconButton>
+      
+      <Divider />
 
-      <List>
+      {/* カレンダーエリア / 開閉可能 */}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          color='transparent'
+          position='static'
+          elevation={0}
+          >
+          <Toolbar
+            variant='dense'
+            className={classes.toolBar}
+          >
+            <IconButton
+              size='small'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
+              sx={{ mr: 2 }}
+            >
+              <KeyboardArrowDownIcon />
+            </IconButton>
+            <Typography variant='subtitle2' component='div' sx={{ flexGrow: 1 }}>
+              2021年 11月
+            </Typography>
+            <IconButton
+              size='small'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
+            >
+              <ArrowUpwardIcon />
+            </IconButton>
+            <IconButton
+              size='small'
+              edge='start'
+              color='inherit'
+              aria-label='menu'
+            >
+              <ArrowDownwardIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: '2px',
+              width: '100%',
+              height: 240,
+            },
+          }}
+        >
+          <Paper elevation={0} />
+        </Box>
+      </Box>
+
+      <Divider />
+
+      {/* <List>
         <ListSubheader inset>Service Menu</ListSubheader>
         <ListItem button>
           <ListItemIcon>
@@ -112,9 +187,10 @@ const ServiceMenu = () => {
           </ListItemIcon>
           <ListItemText primary='Integrations' />
         </ListItem>
-      </List>
+      </List> */}
     </Drawer>
   );
+
 };
 
 export default ServiceMenu;
