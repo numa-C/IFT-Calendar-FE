cca23e98a31d31e004a8659bad86ac8bd71c393a
RyoKainuma <numac.kr@gmail.com>
2021-11-28 11:43:34 +0900

    Removed unused code.

--- src/src/pages/Main.tsx
+++ src/src/pages/Main.tsx
@@ -2,23 +2,10 @@ import * as React from 'react';
 import { makeStyles } from '@mui/styles';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import MenuIcon from '@mui/icons-material/Menu';
 import Dashboard from './Mains/Dashboard';
 import Calendar from './Mains/Calendar';
 
-const useStyles = makeStyles(
-  {
-    toolBar: {
-      maxHeight: 48,
-      minHeight: '36px !important',
-    }
-  }
-);
-
 const  Main = () => {
-  const classes = useStyles();
-
   return (
     <Box
       component='main'
@@ -31,15 +18,6 @@ const  Main = () => {
         overflow: 'auto',
       }}
     >
-      {/* MainHeader for each services */}
-      {/* <Toolbar
-        sx={{
-          pt: 9
-        }}
-        className={classes.toolBar}
-      >
-        <MenuIcon />
-      </Toolbar> */}
       <Container maxWidth='xl' sx={{ mt: 6, mb: 6 }}>
         {/* <Dashboard /> */}
         <Calendar />
